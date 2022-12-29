610f8561ecc9368c26c6d784376e621d58893332
Thinh Pham <devercode@gmail.com>
2022-12-29 17:08:56 +0700

    feat: use curl impersonate

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,29 +1,56 @@
-import { createLogger, TransportType } from "../index";
-describe("features", () => {
-  it("Should throw simple log", () => {
-    const logger = createLogger({
-      transports: [TransportType.CONSOLE],
-    });
-    logger.info("LABEL::Helloworld");
-    logger.warn("Warning!!!");
-    logger.error("throw an error");
-    logger.silly("Silly log");
+import { Curl } from "../curl";
+import path from "path";
+import { Scraper, ScraperRequestError } from "..";
+describe("Curl", () => {
+  const curl = new Curl();
+  const scraper = new Scraper(curl);
+  it("can access blur", async () => {
+    const testUrl = "https://core-api.prod.blur.io/auth/challenge";
+    const res = await scraper.post(
+      testUrl,
+      {
+        walletAddress: "0xdb9ece7aab0ddbb78b59f44b0bef9539cfeaf203",
+      },
+      {
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
+    expect(res.status).toEqual(201);
   });
 
-  it.only("should catch error with sentry", () => {
-    const logger = createLogger({
-      transports: [TransportType.CONSOLE, TransportType.SENTRY],
-      context: {
-        sentryOpts: {
-          level: "error",
-          sentry: {
-            dsn: "https://27a93ba8adce472db368f5ad8c7789f1@o850084.ingest.sentry.io/5816836",
-          },
+  it(`Can use auth proxy`, async () => {
+    scraper.useProxy("http://jnunknlz:bhs71x4u0ay8@45.41.177.87:5737");
+    const { data } = await scraper.get<string>("https://www.showmyip.com");
+    expect(data.includes("45.41.177.87")).toBeTruthy();
+  });
+  it(`can use rotating proxy`, async () => {
+    scraper.useProxy("http://69.46.80.226:8150");
+    const { data } = await scraper.get("https://www.showmyip.com");
+  });
+  it("Can handle proxy error", async () => {
+    scraper.useProxy("http://jnsunknlz:bhs71x4u0ay8@45.41.177.87:5737");
+    await expect(scraper.get("https://www.showmyip.com")).rejects.toThrow();
+  });
+
+  it("can handle bad request", async () => {
+    scraper.useProxy("http://jnunknlz:bhs71x4u0ay8@192.241.94.76:7631");
+    const testUrl = "https://core-api.prod.blur.io/auth/challenge";
+    try {
+      await scraper.post(
+        testUrl,
+        {
+          walletAddress: "wrong addres",
         },
-      },
-    });
-    logger.error("Throw an error", {
-      foo: "foo",
-    });
+        {
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      );
+    } catch (err) {
+      expect(err instanceof ScraperRequestError).toBeTruthy();
+    }
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,105 @@
-import errorParser from "error-stack-parser";
+export { Curl } from "./curl";
 
-import moment from "moment";
-import winston from "winston";
-import Sentry from "winston-transport-sentry-node";
-import chalk from "chalk";
-const colors = {
-  info: "cyan",
-  warn: "yellow",
-  error: "red",
-  debug: "blue",
-  silly: "magenta",
-};
-const colorizer = winston.format.colorize({
-  colors,
-});
-
-const consoleLogFormat = winston.format.combine(
-  winston.format.timestamp(),
-  winston.format.simple(),
-  winston.format.metadata(),
-  winston.format.splat(),
-  winston.format.printf((msg) => {
-    const args = msg.message.split("::");
-    let label, message;
-    if (args.length > 1) {
-      label = args[0].toUpperCase();
-      message = args[1];
-    } else {
-      label = msg.level.toUpperCase();
-      message = msg.message;
-    }
-    return `${chalk.bgKeyword(colors[msg.level]).keyword("black")(
-      `[${label}]`
-    )}: ${message} ${chalk.dim.keyword("black")(
-      `[${moment(msg.timestamp).format("DD/MM/YY hh:mm:ss")}]`
-    )}`;
-  })
-);
-
-const productionFormat = winston.format.combine(
-  winston.format.timestamp(),
-  winston.format.simple(),
-  winston.format.splat(),
-  winston.format.printf(
-    (msg) =>
-      `${moment
-        .utc(msg.timestamp)
-        .utcOffset(7)
-        .format("DD/MM/YYYY hh:mm:ss")} - ${msg.level}: ${msg.message}`
-  )
-);
-
-export enum TransportType {
-  FILE = "FILE",
-  CONSOLE = "CONSOLE",
-  SENTRY = "SENTRY",
+export interface DvRequestOpts {
+  headers?: Record<string, any>;
 }
 
-export type GetTransportContext = {
-  sentryOpts?: {
-    sentry: {
-      dsn: string;
-    };
-    level: string;
-  };
-};
+export class ScraperRequestError {
+  status: number;
+  headers: Record<any, string>;
+  data: any;
+  constructor({ status, headers, data }) {
+    this.status = status;
+    this.headers = headers;
+    this.data = data;
+  }
+}
+export class ScraperError extends Error {}
+export interface IScraper {
+  get<T>(
+    url: string,
+    opts?: DvRequestOpts
+  ): Promise<{
+    data: T;
+    headers: Record<string, unknown>;
+    status: number;
+  }>;
+  post<T>(
+    url: string,
+    data: Record<string, any>,
+    opts?: DvRequestOpts
+  ): Promise<{
+    data: T;
+    headers: Record<string, unknown>;
+    status: number;
+  }>;
+}
 
-const getTransports = (
-  supportedTransports: TransportType[],
-  context: GetTransportContext
-) => {
-  const transports = [];
-  supportedTransports.forEach((transportType) => {
-    switch (transportType) {
-      case TransportType.FILE: {
-        transports.push(
-          new winston.transports.File({
-            level: "error",
-            filename: "logs/errors.log",
-            format: productionFormat,
-          })
-        );
-        break;
-      }
-      case TransportType.CONSOLE: {
-        transports.push(
-          new winston.transports.Console({
-            format: consoleLogFormat,
-            level: "silly",
-          })
-        );
-        break;
-      }
-      case TransportType.SENTRY: {
-        const sentry = new Sentry(context.sentryOpts);
-        transports.push(sentry);
-        break;
-      }
-    }
-  });
-  return transports;
-};
+export interface IScraperAdapter {
+  proxy?: string;
+  userAgent?: string;
+  get<T>(
+    url: string,
+    opts?: DvRequestOpts
+  ): Promise<{
+    data: T;
+    headers: Record<string, unknown>;
+    status: number;
+  }>;
+  post<T>(
+    url: string,
+    data: Record<string, any>,
+    opts?: DvRequestOpts
+  ): Promise<{
+    data: T;
+    headers: Record<string, unknown>;
+    status: number;
+  }>;
+}
+export class Scraper implements IScraper {
+  private adapter: IScraperAdapter;
+  proxy?: string;
+  private ja3?: string;
+  private userAgent: string;
+  constructor(adapter: IScraperAdapter) {
+    this.adapter = adapter;
+  }
 
-const _parserError = (err: Error) => {
-  const errors = errorParser.parse(err);
-  const firstStack = errors[0];
-  return {
-    message: err.message,
-    errorAt: `${firstStack.fileName}:${firstStack.lineNumber}:${firstStack.columnNumber}`,
-  };
-};
+  get<T>(
+    url: string,
+    opts?: DvRequestOpts
+  ): Promise<{
+    data: T;
+    headers: Record<string, unknown>;
+    status: number;
+  }> {
+    return this.adapter.get(url, {
+      ...opts,
+      headers: {
+        ...(opts?.headers ? opts.headers : {}),
+      },
+    });
+  }
 
-export const createLogger = ({
-  transports,
-  context,
-}: {
-  transports: TransportType[];
-  context?: GetTransportContext;
-}) => {
-  return winston.createLogger({
-    transports: getTransports(transports, context),
-    level: "silly",
-  });
-};
+  post<T>(
+    url: string,
+    data: Record<string, any>,
+    opts?: DvRequestOpts
+  ): Promise<{
+    data: T;
+    headers: Record<string, unknown>;
+    status: number;
+  }> {
+    return this.adapter.post(url, data, {
+      ...opts,
 
-export const registerErrorHandling = ({
-  logger,
-  isForceQuitWhenErrorOccur,
-}: {
-  logger: winston.Logger;
-  isForceQuitWhenErrorOccur: boolean;
-}) => {
-  logger.exitOnError = false;
-  process.on("unhandledRejection", async (err: Error) => {
-    const error = _parserError(err);
-    logger.error(
-      "UN_HANDLED_ERROR: message: %s, Error At: %s \n",
-      error.message,
-      error.errorAt
-    );
-    if (isForceQuitWhenErrorOccur) {
-      console.log("error detected ! graceful exit in 7 seconds");
-      setTimeout(() => {
-        process.exit(1);
-      }, 7000);
-    } else {
-      return;
-    }
-  });
-  process.on("uncaughtException", async (err) => {
-    const error = _parserError(err);
-    logger.error(
-      "UN_CAUGHT_ERROR: message: %s, Error At: %s",
-      error.message,
-      error.errorAt
-    );
-    if (isForceQuitWhenErrorOccur) {
-      console.log("error detected ! graceful exit in 7 seconds");
-      setTimeout(() => {
-        process.exit(1);
-      }, 7000);
-    } else {
-      return;
-    }
-  });
-};
+      headers: {
+        ...(opts.headers ? opts.headers : {}),
+      },
+    });
+  }
+
+  useProxy(proxy: string) {
+    this.adapter.proxy = proxy;
+  }
+}
